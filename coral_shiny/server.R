88b4d02c1b4473cf4c1be30a97a70ae1db3d488e
fawda123 <mbafs2012@gmail.com>
2016-12-02 17:38:38 -0600

    better shiny app with leaflet

--- coral_shiny/server.R
+++ coral_shiny/server.R
@@ -1,6 +1,6 @@
 library(tidyverse)
-library(readxl)
-library(ggmap)
+library(leaflet)
+library(RColorBrewer)
 
 data(crl_abu)
 
@@ -8,64 +8,133 @@ data(crl_abu)
 shinyServer(function(input, output, session) {
   
   ## dynamic UI
-  
-  # run model
-  runmod <- eventReactive(input$runmod, {
+
+  # data to plot
+  dat <- reactive({
     
-    # progress
-    progress <- shiny::Progress$new(session, min=1, max=1)
-    progress$set(message = 'FishTank is running...')
-    on.exit(progress$close())
-
-    # format parameter inputs 
-    parsin <- form_parinps(input)
-
-    # format initial condition inputs
-    iniin <- form_iniinps(input)
-
-    # p1z1 switch
-    p1z1 <- input$p1z1
-
-    # run model
-    run_mod(pars = parsin, inps = iniin, out_var = NULL,  p1z1 = p1z1)
-      
-  })
-
-  # first variable plot
-  output$map <- renderPlot({
-     
-    # input
-    left <- input$left
-    bottom <- input$bottom
-    right <- input$right
-    top <- input$top
-    log <- input$log
     spp <- input$spp
-    
-    # extent
-    ext <- c(left, bottom, right, top)
-    map <- get_stamenmap(ext, zoom = 10, maptype = "toner-lite")
     
     # select data
     names(crl_abu)[names(crl_abu) %in% spp] <- 'spp'
-    toplo <- select(crl_abu, lon, lat, spp)
+    toplo <- select(crl_abu, Station, lon, lat, spp)
+    toplo
     
-    ylab <- 'Richness'
-    if(spp != 'Richness')
-      ylab <- 'Abundance'
+  })
+
+  # This reactive expression represents the palette function,
+  # which changes as the user makes selections in UI.
+  colorpal <- reactive({
+    pal <- brewer.pal(11, 'Spectral')
+    colorNumeric(pal, dat()$spp)
+  })
+  
+  # map for abundance
+  output$map <- renderLeaflet({
+
+    # base map for spp abu
+    leaflet(crl_abu) %>% addTiles() %>%
+      fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
     
-    p <- ggmap(map) + 
-      geom_point(data = toplo, aes(x = lon, y = lat, size = spp, fill = spp), pch = 21, alpha = 0.8) + 
-      scale_fill_distiller(palette = 'Spectral', direction = 1, guide = guide_legend(title = ylab)) +
-      scale_size(range = c(1, 12), guide = guide_legend(title = ylab)) + 
-      theme_bw() + 
-      theme(
-        axis.title.x = element_blank(), 
-        axis.title.y = element_blank()
+  })
+  
+  # map for stations
+  output$mapstat <- renderLeaflet({
+
+    # base map for stations
+    leaflet(crl_abu) %>% addTiles() %>%
+      fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat)) %>% 
+      addCircleMarkers(radius = 10, weight = 0, fillOpacity = 0, label = ~paste0('Station ', Station),
+        layerId = ~Station
+        ) %>% 
+      addLabelOnlyMarkers(~lon, ~lat, 
+        label = ~paste0(Station), 
+        labelOptions = labelOptions(noHide = T, direction = 'top', textOnly = T)
+        )
+      
+  })
+
+  # points on abu map
+  observe({
+    
+    pal <- colorpal()
+    pt_scl <- input$pt_scl
+
+    leafletProxy("map", data = dat()) %>%
+      clearMarkers() %>%
+      clearControls() %>% 
+      addCircleMarkers(radius = ~ pt_scl * spp, weight = 1, color = "#777777",
+        fillColor = ~pal(spp), fillOpacity = 0.7, label = ~paste0('Station ', Station, ': ', spp)
+      ) %>% 
+      addLegend(position = "bottomleft",
+        pal = pal, values = ~spp, title = 'Count'
       )
+    
+  })
+    
+  output$abuplot <- renderPlot({
+    
+    toplo <- dat() %>% 
+      arrange(spp) %>% 
+      mutate(Station = factor(Station, levels = Station[order(spp)]))
+    
+    p <- ggplot(toplo, aes(x = Station, y = spp, fill = spp)) + 
+      geom_bar(stat = 'identity') + 
+      theme_minimal() +
+      scale_fill_gradientn(colours = brewer.pal(11, 'Spectral')) +
+      ylab('Coral counts at each station') + 
+      theme(legend.position = "none", 
+        axis.text.x = element_text(size = 8), 
+        axis.line.x = element_line(), 
+        axis.line.y = element_line(), 
+        axis.ticks.x = element_line(),
+        axis.ticks.y = element_line(),
+        axis.ticks.length = unit(.1, "cm")
+        )
     
     print(p)
     
-    })
+  })
+  
+  ## get station on map click
+  
+  # bindings for station click 
+  makeReactiveBinding('selectedstation')
+  
+  # Set selected station when marker is clicked
+  observeEvent(input$mapstat_marker_click, {
+    selectedstation <<- input$mapstat_marker_click$id
+  })
+  
+  # Clear the selected station when map background is clicked
+  observeEvent(input$mapstat_click, {
+    selectedstation <<- NULL
+  })
+  
+  output$staplot <- renderPlot({
+    
+    # check if its been clicked
+    validate(need(selectedstation, FALSE)) 
 
+    toplo2 <- filter(crl_abu, Station %in% selectedstation) %>% 
+      gather('Species', 'Count', -Station, -lat, -lon, -Richness) 
+    p <- ggplot(toplo2, aes(x = Species, y = Count, fill = Count)) + 
+      geom_bar(stat = 'identity') + 
+      theme_minimal() +
+      scale_fill_gradientn(colours = brewer.pal(11, 'Spectral')) +
+      theme(legend.position = "none", 
+        axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 8), 
+        axis.title.x = element_blank(),
+        axis.line.x = element_line(), 
+        axis.line.y = element_line(), 
+        axis.ticks.x = element_line(),
+        axis.ticks.y = element_line(),
+        axis.ticks.length = unit(.1, "cm")
+        ) + 
+      ggtitle(paste('Station', selectedstation))
+    
+    print(p)
+    
+  })
+  
+  
 })
--- coral_shiny/ui.R
+++ coral_shiny/ui.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(leaflet)
+library(RColorBrewer)
 library(shinythemes)
 
 # data
@@ -11,64 +13,72 @@ vrs <- sort(vrs[!vrs %in% c('Station', 'lat', 'lon')])
 # Define UI for application
 shinyUI(fluidPage(
   
-  theme = shinytheme('journal'), 
-  
-  # main panel
-  mainPanel(width = 12,
-      
-    # spacing
-    fluidRow(HTML('<p><h3>Puerto Rico, 2011 stony coral surveys </h3></p>')),
+  theme = shinytheme('journal'),
+
+  tabsetPanel(
     
-    #spacing
-    fluidRow(HTML('<p></p>')),
+    tabPanel('By species', 
       
-    # first row of plots
-    fluidRow(
+      leafletOutput("map", width = "100%", height = 500),
       
-      column(width = 4, 
-        selectInput(inputId = 'spp', 
-          label = h4('Select species'),
-          choices = vrs,
-          selected = 'Richness'
-          )
-        ),
-      
-      column(width = 2, 
-        numericInput(inputId = 'left', 
-          label = h4('left'),
-          value = -67.43174
-          )
-        ),
-      
-      column(width = 2, 
-        numericInput(inputId = 'bottom', 
-          label = h4('bottom'),
-          value = 17.57098
-          )
-        ),
-      
-      column(width = 2, 
-        numericInput(inputId = 'right', 
-          label = h4('right'),
-          value = -65.53003
-          )
-        ),
-      
-      column(width = 2, 
-        numericInput(inputId = 'top', 
-          label = h4('top'),
-          value = 18.58847
-          )
-        ),
-      
-      column(width = 12, 
+      # main panel
+      column(width = 12,
+          
+        # spacing
+        fluidRow(HTML('<p><h3>Puerto Rico, 2011 stony coral surveys </h3></p>')),
         
-        plotOutput("map", width = '1200px', height = '750px')
+        #spacing
+        fluidRow(HTML('<p></p>')),
+          
+        # first row of plots
+        fluidRow(
+          
+          column(width = 4, 
+            
+            selectInput(inputId = 'spp', 
+              label = h4('Select species'),
+              choices = vrs,
+              selected = 'Richness'
+              ),
+            
+            sliderInput("pt_scl", 
+              label = "Point scales", 
+              min = 1, 
+              max = 10,
+              value = 2, 
+              step = 0.5
+              )
+            
+            ),
+          
+          column(width = 8, 
+           
+            plotOutput('abuplot', width='100%', height='300px')
+            
+            )
         
+          )
+    
         )
       
-      ) 
+      ), 
+    
+    tabPanel('By site',
+    
+      leafletOutput("mapstat", width = "100%", height = 500),
+      
+      # main panel
+      column(width = 12,
+          
+        # spacing
+        fluidRow(HTML('<p><h3>Click a station</h3></p>')),
+        
+        plotOutput('staplot', width='100%', height='300px')
+       
+      )
     
     )
-        
+  
+  )  
+
 ))
