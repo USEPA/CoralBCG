29561fcd9045eaaae732d5c807fec663de438b33
fawda123 <mbafs2012@gmail.com>
2017-02-14 07:52:09 -0600

    better handling of recruits for csa

--- R/funcs.R
+++ R/funcs.R
@@ -7,6 +7,8 @@
 # d2 numeric of colony diameter (dm), diameter of minor axis if ellipse
 #
 # if both d1 and d2 are provided, they are converted to a single value for the diameter of a circle with the same area as the ellipse
+#
+# NA values will be all that don't match species in conv, juveniles marked as -1
 est_3d <- function(spp, h, d1, d2 = d1){
   
   data(conv)
@@ -20,12 +22,15 @@ est_3d <- function(spp, h, d1, d2 = d1){
 
   # use size conversions if not in estfun
   csa <- data.frame(spec = spp, h = h, d = d, stringsAsFactors = FALSE) %>% 
-    left_join(., unique(conv), by = 'spec') %>% 
+    left_join(., conv, by = 'spec') %>% 
     rename(M = conv) %>% 
     mutate(
       csa = pi * ((h + d / 2) / 2) ^ 2 * M
       )
 
+  # juvies back to -1
+  csa$csa[d1 == -1] <- -1
+  
   out <- csa$csa
   return(out)
   
