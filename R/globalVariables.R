5db5173e8f01535e3f3f44df073d5ad4ef9ae0ac
fawda123 <mbafs2012@gmail.com>
2017-06-15 12:16:06 -0500

    docs updated

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,7 +1,7 @@
 globalVariables(c('.', 'Bleached', 'ColonyID', 'Diseased', 'Height', 'M', 'MaxDiam', 'MortNew', 'MortOld',
   'PerpDiam', 'Recruits', 'conv', 'crl_dem', 'csa', 'lcsa', 'lrg_mort', 'lrg_pa', 'species_name',
   'spp_abu', 'station_code', 'val', 'var', 'bcg', 'dnorm', 'est', 'ests', 'na.omit', 'pr', 'rsums', 'scr', 
-  'vals', 'mu', 'prb', 'scr_act', 'sd'))
+  'vals', 'mu', 'prb', 'scr_act', 'sd', 'mins'))
 
 #' @importFrom stats dnorm na.omit sd var
 NULL
